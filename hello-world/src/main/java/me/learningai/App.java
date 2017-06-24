0f5474d02002605b81fa4733c6d3ed5fcc07c816
heyx-any <heyx.any@gmail.com>
2017-06-24 15:03:11 +0800

    hello world

---
+++ hello-world/src/main/java/me/learningai/App.java
@@ -0,0 +1,15 @@
+package me.learningai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot启动类.
+ * @author heyx
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
--- hello-world/src/main/java/me/learningai/controller/HelloWorldController.java
+++ hello-world/src/main/java/me/learningai/controller/HelloWorldController.java
@@ -1,7 +1,35 @@
+/*
+ * Copyright (c)  2017 http://www.learningai.me
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package me.learningai.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
+ * hello world.
+ *
  * @author heyx
  */
+@RestController
+@RequestMapping("/hello-world")
 public class HelloWorldController {
+    @GetMapping
+    public String sayHello() {
+        return "hello,world";
+    }
 }
